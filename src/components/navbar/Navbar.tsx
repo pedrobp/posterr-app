fa0ad11366fe18cde5fce9609af725799c5162cf
Pedro <pedro.b.pimenta1@gmail.com>
2022-03-23 20:27:40 -0300

    Fixes navbar redirect

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: FC = () => {
           <Button
             variant="raw"
             className="font-semibold"
-            onClick={() => navigate('/home')}
+            onClick={() => navigate('/all')}
           >
             Home
           </Button>
