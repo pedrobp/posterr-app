172103766c5514b73a5a30a71276736249a1a3fc
Pedro <pedro.b.pimenta1@gmail.com>
2022-03-24 00:47:36 -0300

    Adds api mock file for demonstration

---
+++ src/api/apiMock.ts
@@ -0,0 +1,34 @@
+// import axios from 'axios'
+// import { Post } from 'types'
+
+// const api = axios.create({
+//   baseURL: 'API_URL',
+// })
+
+// const getAllPosts = () =>
+//   api
+//     .get('/posts')
+//     .then((r) => setPosts(r.data))
+//     .catch((e) => {
+//       console.error(e)
+//     })
+
+// const getPost = (id: ID) =>
+//   api
+//     .get(`/posts/${id}`)
+//     .then((r) => setPost(r.data))
+//     .catch((e) => {
+//       console.error(e)
+//     })
+
+// const addPost = (post: Post) => api.post('/posts', post)
+
+// const getUser = (id: ID) =>
+//   api
+//     .get(`/users/${id}`)
+//     .then((r) => setUser(r.data))
+//     .catch((e) => {
+//       console.error(e)
+//     })
+
+export const ApiMock = {}
--- src/api/post.ts
+++
@@ -1,3 +0,0 @@
-export const getPosts = () => {
-  console.log('test')
-}
